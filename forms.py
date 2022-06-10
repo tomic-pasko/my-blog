bdba7ee9646e2131929633b25599a13db405a157
pasko.tomic <tomicpasko@gmail.com>
2022-06-10 15:28:26 +0200

    email, url validator removed

--- forms.py
+++ forms.py
@@ -9,20 +9,20 @@ from flask_ckeditor import CKEditorField
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
 
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(),  email()])
+    email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("Sign me up")
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), email()])
+    email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Log in")
 
